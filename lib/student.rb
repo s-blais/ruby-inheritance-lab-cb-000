c733092d08f9870103a7f4e414b99bf08e10c63b
Stephen Blais <stephen@s-blais.com>
2019-09-04 19:53:59 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,28 @@
-class Student 
+class Student < User
+
+  attr_accessor :knowledge
+
+  @@all = []
+
+  def initialize
+    @knowledge = []
+    @@all << self
+  end
+
+  def learn(string)
+    @knowledge << string
+  end
+
+  def knowledge
+    @knowledge
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.all_student_knowledge
+    self.all.each {|pupil| puts "#{pupil.first_name}: #{pupil.knowledge}"}
+  end
 
 end
